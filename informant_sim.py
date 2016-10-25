341feb73dad7463cc3a48d10efa0f23e574c95d1
Noah DeMarco <noah.demarco@gmail.com>
2016-10-24 23:02:18 -0400

    Print colored wires and wire states

--- informant_sim.py
+++ informant_sim.py
@@ -1,38 +1,54 @@
 #!/usr/bin/python
 import random
+import sys
 
-print "-----------------"
-print "-- BOMB DEFUSE --"
-print "-----------------"
+print '-----------------'
+print '-- BOMB DEFUSE --'
+print '-----------------'
 
 # 5 Rounds, each round has:
 #  1 bad wire
 #  1-2 good wires
 #  0-1 dead wires
-num_rounds = 5;
+num_rounds = 5
+color_ids = [1, 2, 4, 5, 7]
+wire_colors = {1:'red', 2:'green', 4:'blue', 5:'purple', 7:'white'}
+wire_states = ['good', 'dead', 'bad']
 
 # Assume 5 players for now
 # Game roles:
 #  1 Spy
 #  4 Informants
-players = ["Brett", "Noah", "Joelle", "Mark", "Jack"];
-random.shuffle(players);
-spy = players[0];
+players = ['Brett', 'Noah', 'Joelle', 'Mark', 'Jack']
+random.shuffle(players)
+spy = players[0]
 informants = players[1:]
-print "{} is the spy\n".format(spy)
+print '{} is the spy\n'.format(spy)
 
 # Shuffle again and we'll rotate who's being the defuser
-random.shuffle(players);
+random.shuffle(players)
 for i in range(num_rounds):
+    print 'Round {}:'.format(i)
+
+    # Choose the defuser for the round
+    defuser = players[i % len(players)]
+    print '  {} is the defuser'.format(defuser)
+
+    # Choose the wires - 3 per round for now
+    random.shuffle(color_ids)
+    colors = color_ids[0:3]
+    print '  Wires: \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m'.format(colors[0], colors[1], colors[2])
+    random.shuffle(wire_states)
+    print '         {} {} {}'.format(wire_states[0][0], wire_states[1][0], wire_states[2][0])
+
     # Round roles:
     #  1   Defuser
     #  0-1 Spies
     #  2-3 Informants
     #  1   Misinformed
-    defuser = players[i % len(players)];
-    round_spies = [];
-    round_informants = [];
-    round_misinformed = [];
+    round_spies = []
+    round_informants = []
+    round_misinformed = []
     random.shuffle(informants)
     if defuser == spy:
         round_misinformed.extend(informants[0:2])
@@ -44,11 +60,9 @@ for i in range(num_rounds):
         round_informants.extend(informants[1:])
         informants.append(defuser)
 
-    print "Round {}:".format(i)
-    print "  {} is the defuser".format(defuser)
     for inf in round_informants:
-        print "  {} is informed".format(inf)
+        print '  {} is informed'.format(inf)
     for minf in round_misinformed:
-        print "  {} is misinformed".format(minf)
-    print ""
+        print '  {} is misinformed'.format(minf)
+    sys.stdin.readline()
 
