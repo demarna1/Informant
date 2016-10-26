cfe066a509074a8dddb25c5cf0859fd03a5c0e12
Noah DeMarco <noah.demarco@gmail.com>
2016-10-25 22:00:09 -0400

    Add wiring cutting to the simulation

--- informant_sim.py
+++ informant_sim.py
@@ -6,6 +6,8 @@ print '-----------------'
 print '-- BOMB DEFUSE --'
 print '-----------------'
 
+debug = False
+
 # 5 Rounds, each round has:
 #  1 bad wire
 #  1-2 good wires
@@ -23,12 +25,13 @@ players = ['Brett', 'Noah', 'Joelle', 'Mark', 'Jack']
 random.shuffle(players)
 spy = players[0]
 informants = players[1:]
-print '{} is the spy\n'.format(spy)
+if debug:
+    print '{} is the spy\n'.format(spy)
 
 # Shuffle again and we'll rotate who's being the defuser
 random.shuffle(players)
 for i in range(num_rounds):
-    print 'Round {}:'.format(i)
+    print 'Round {}:'.format(i+1)
 
     # Choose the defuser for the round
     defuser = players[i % len(players)]
@@ -39,7 +42,8 @@ for i in range(num_rounds):
     colors = color_ids[0:3]
     print '  Wires: \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m'.format(colors[0], colors[1], colors[2])
     random.shuffle(wire_states)
-    print '         {} {} {}'.format(wire_states[0][0], wire_states[1][0], wire_states[2][0])
+    if debug:
+        print '         {} {} {}'.format(wire_states[0][0], wire_states[1][0], wire_states[2][0])
 
     # Populate the possible hints
     for index, state in enumerate(wire_states):
@@ -80,11 +84,40 @@ for i in range(num_rounds):
         round_informants.extend(informants[1:])
         informants.append(defuser)
 
-    for inf in round_informants:
-        print '  {} is informed, thinks the {}'.format(inf, random.choice(informed_hints))
-    for minf in round_misinformed:
-        print '  {} is misinformed, thinks the {}'.format(minf, random.choice(misinformed_hints))
-    for s in round_spies:
-        print '  {} is the spy, knows the {}'.format(s, spy_hint)
-    sys.stdin.readline()
+    # The players tell the defuser which wire should be cut
+    if debug:
+        for inf in round_informants:
+            print '    {} is informed, thinks the {}'.format(inf, random.choice(informed_hints))
+        for minf in round_misinformed:
+            print '    {} is misinformed, thinks the {}'.format(minf, random.choice(misinformed_hints))
+        for s in round_spies:
+            print '    {} is the spy, knows the {}'.format(s, spy_hint)
+    else:
+        round_players = []
+        for inf in round_informants:
+            round_players.append((inf, random.choice(informed_hints)))
+        for minf in round_misinformed:
+            round_players.append((minf, random.choice(misinformed_hints)))
+        for s in round_spies:
+            round_players.append((s, random.choice(misinformed_hints)))
+        random.shuffle(round_players)
+        for rp in round_players:
+            print '    {} thinks the {}'.format(rp[0], rp[1])
+
+    # The defuser cuts wires until the good wire neutralizes the bomb
+    # or the bad wire blows up the bomb
+    round_finished = False
+    while not round_finished:
+        wire_to_cut = raw_input("  Wire to cut: ")
+        if wire_to_cut[0] == wire_colors[good_wire][0]:
+            print "  You cut the {} wire, which neutralized the bomb, congrats!".format(wire_colors[good_wire])
+            round_finished = True
+        elif wire_to_cut[0] == wire_colors[dead_wire][0]:
+            print "  You cut the {} wire, which did nothing, cut again...".format(wire_colors[dead_wire])
+        elif wire_to_cut[0] == wire_colors[bad_wire][0]:
+            print "  You cut the {} wire, which blew up the bomb :(".format(wire_colors[bad_wire])
+            round_finished = True
+        else:
+            print "  You typed an invalid color, cut again..."
+    raw_input("  ENTER to continue\n")
 
