314f2ad27daf4a7bc52ddc18eca9482c76431c89
Noah DeMarco <noah.demarco@gmail.com>
2016-11-06 21:04:49 -0500

    Refactor homepage code out of informant directory

--- informant/inf_server.js
+++ informant/inf_server.js
@@ -1,14 +1,5 @@
-/*
- * Informant game server
- */
-var express = require('express');
-
+// Informant game server
 exports.socketServer = function(app, server) {
-    var routes = require('./main/routes');
-    app.use(express.static(__dirname + '/main'));
-    app.use(express.static(__dirname + '/game'));
-    routes(app);
-
     var io = require('socket.io')(server);
     var infNamespace = io.of('/informant');
     infNamespace.on('connection', function(socket) {
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-/*
- * Global site server
- */
+// Entry point - create node.js server with Express
 var express = require('express');
 var app = express();
 var nodeServer = require('http').createServer(app);
-var nodePort = process.env.PORT || 3000;
 
+// Configure site routes
+var path = require('path');
+app.use(express.static(__dirname + '/main'));
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('main/index.html'));
+});
+app.use(express.static(__dirname + '/informant/game'));
+app.get('/informant', function(req, res) {
+    res.sendFile(path.resolve('informant/game/practice.html'));
+});
+//app.use(express.static(__dirname + '/cah/game'));
+//app.get('/cah', function(req, res) {
+    //res.sendFile(path.resolve('cah/game/game.html'));
+//});
+
+// Start server
+var nodePort = process.env.PORT || 3000;
 nodeServer.listen(nodePort, function() {
     console.log('Node.js server listening on port ' + nodePort);
 });
 
+// Create socket.io server for each game
 var infServer = require('./informant/inf_server.js');
 //var cahServer = require('./cah/cah_server.js');
 infServer.socketServer(app, nodeServer);
