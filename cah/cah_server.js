fb6eb9572594f8113c09524d421f1f89a0249cf2
Noah DeMarco <noah.demarco@gmail.com>
2016-11-15 01:12:11 -0500

    Port CAH - generate game code

--- cah/cah_server.js
+++ cah/cah_server.js
@@ -3,5 +3,23 @@ exports.addListener = function(io) {
     var infNamespace = io.of('/cah');
     infNamespace.on('connection', function(socket) {
         console.log('new CAH connection');
+        socket.emit('connected');
+
+        function generateGameCode() {
+            var text = '';
+            var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            for (var i=0; i<4; i++) {
+                text += letters.charAt(Math.floor(Math.random() * letters.length));
+            }
+            return text;
+        }
+
+        socket.on('new game', function() {
+            var gameCode = generateGameCode();
+            console.log('New CAH game with game code = ' + gameCode);
+            socket.emit('code created', {
+                gameCode: gameCode
+            });
+        });
     });
 };
--- cah/game/cah_game.js
+++ cah/game/cah_game.js
@@ -1,4 +1,19 @@
 $(function() {
-    console.log('creating CAH socket');
+    // Other jQuery elements
+    var $lobbyList = $('.lobbyList');
+    var $gameCode = $('.code');
+
+    // State variables
     var socket = io('/cah');
+    var state = null;
+
+    socket.on('connected', function() {
+        socket.emit('new game');
+    });
+
+    socket.on('code created', function(data) {
+        state = new State(data.gameCode);
+        $gameCode.text(state.gameCode);
+        $lobbyList.empty();
+    });
 });
