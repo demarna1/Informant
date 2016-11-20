251fa6809e79a29fdb2347b2deedf18d2b71d34c
Noah DeMarco <noah.demarco@gmail.com>
2016-11-20 00:44:57 -0500

    Port CAH - update lobby with added players

--- cah/game/cah_game.js
+++ cah/game/cah_game.js
@@ -1,11 +1,27 @@
 $(function() {
     // Other jQuery elements
-    var $lobbyList = $('.lobbyList');
     var $gameCode = $('.code');
+    var $lobbyList = $('.lobbyList');
+    var $readyLabel = $('.readyLabel .label');
+    var $startButton = $('.startButton .button');
 
     // State variables
     var socket = io('/cah');
     var state = null;
+
+    function updateLobby() {
+        $lobbyList.empty();
+        for (var i = 0; i < state.players.length; i++) {
+            $lobbyList.append('<li class="lobbyPlayer">' + state.players[i].username + '</li>');
+        }
+        if (state.players.length >= 2) {
+            $readyLabel.text('All players ready?');
+            $startButton.removeAttr('disabled');
+        } else {
+            $readyLabel.text('Need ' + (2 - state.players.length) + ' more player(s).');
+            $startButton.attr('disabled', 'disabled');
+        }
+    }
 
     socket.on('connected', function() {
         socket.emit('new game');
@@ -20,5 +36,6 @@ $(function() {
     socket.on('user joined', function(data) {
         state.addUser(data.username);
         console.log(data.username + ' joined, numPlayers = ' + state.players.length);
+        updateLobby();
     });
 });
--- cah/play/play.js
+++ cah/play/play.js
@@ -1,10 +1,30 @@
 $(function() {
+    // Other jQuery elements
+    var $welcomeLabel = $('.welcomeLabel .label');
+    var $waitingLabel = $('.waitingLabel .label');
+
     // State variables
     var socket = io('/cah');
+    var username = '';
+
+    function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1));
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
 
     socket.on('connected', function() {
+        username = getUrlParameter('name');
         socket.emit('user joined', {
-            username: 'NoahTest'
+            gameCode: getUrlParameter('gameCode'),
+            username: username
         });
+        $welcomeLabel.text('Welcome, ' + username + '!');
+        $waitingLabel.text('Waiting to start a new game...');
     });
 });
