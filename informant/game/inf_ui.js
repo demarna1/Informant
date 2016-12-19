446e2bea04a29d4fcda146f3dae6c21da0facf2f
Noah DeMarco <noah.demarco@gmail.com>
2016-12-18 23:04:56 -0500

    Update lobby image quality

--- informant/game/inf_ui.js
+++ informant/game/inf_ui.js
@@ -1,15 +1,18 @@
 function loadGame() {
     var lobbyBackground = null;
+    var lobbyBomb = null;
     var canvas = document.getElementById('infCanvas');
 
     var queue = new createjs.LoadQueue();
     queue.on('complete', handleComplete, this);
     queue.loadManifest([
-        { id: 'lobbyBackground', src: '/InformantLobby.png' }
+        { id: 'lobbyBackground', src: '/LobbyBackground.png' },
+        { id: 'lobbyBomb', src: '/LobbyBomb.png' }
     ]);
 
     function handleComplete() {
         lobbyBackground = queue.getResult('lobbyBackground');
+        lobbyBomb = queue.getResult('lobbyBomb');
         window.addEventListener('resize', resizeCanvas);
         resizeCanvas();
     }
@@ -28,12 +31,19 @@ function loadGame() {
         var scaleWidth = canvas.width / lobbybg.image.width;
         var scaleHeight = canvas.height / lobbybg.image.height;
         var scale = scaleWidth > scaleHeight ? scaleWidth : scaleHeight;
-        console.log('scale = ' + scale);
         lobbybg.scaleX = scale;
         lobbybg.scaleY = scale;
         lobbybg.y = 0;
         lobbybg.x = 0;
         stage.addChild(lobbybg);
+
+        // Draw lobby title and bomb
+        var lobbybomb = new createjs.Bitmap(lobbyBomb);
+        lobbybomb.scaleX = 0.15;
+        lobbybomb.scaleY = 0.15;
+        lobbybomb.y = 40;
+        lobbybomb.x = 40;
+        stage.addChild(lobbybomb);
 
         stage.update();
     }
