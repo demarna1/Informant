b33500e8c058d7a7b3d1e4ec79acf410d6fd336a
Noah DeMarco <noah.demarco@gmail.com>
2017-01-08 17:30:54 -0500

    Simulator - Determine round roles using variable misinformed rate

--- sim/informant_sim.py
+++ sim/informant_sim.py
@@ -6,7 +6,6 @@ import random
 log = logging.getLogger()
 
 # Game roles: either spy or informant
-#
 #                      misinformed  effective
 #  players    spies       rate        rate
 #     4         1        3 @ 25%       75%
@@ -19,7 +18,6 @@ def getPlayersAndRoles(numPlayers):
     # Create player list
     playerList = ['Noah', 'Joelle', 'Brett', 'Jackie', 'Mark', 'Bre', 'John', 'Elena']
     players = playerList[:numPlayers]
-    random.shuffle(players)
 
     # Role probabilities
     roleRates = {
@@ -31,26 +29,80 @@ def getPlayersAndRoles(numPlayers):
     }
 
     # Choose spies and informants
+    random.shuffle(players)
     roleRate = random.choice(roleRates[numPlayers])
     numSpies = roleRate[0]
     roles = {}
-    for p in players[:numSpies]:
-        roles[p] = 'spy'
-    for p in players[numSpies:]:
-        roles[p] = 'informant'
-    log.debug('spies = {}, informants = {}'.format(players[:numSpies], players[numSpies:]))
-    return (players, roles, roleRate[1])
+    roles['spies'] = players[:numSpies]
+    roles['informants'] = players[numSpies:]
+
+    # Choose defuser order
+    random.shuffle(players)
+    roles['defusers'] = players
+    return (roles, roleRate[1])
+
+
+# Round roles:
+#  1 Defuser
+#  Spies
+#  Informants
+#  Misinformed according to misRate
+def getRoundRoles(round, roles, misRate):
+    # Get the round defuser
+    numDefuser = round % len(roles['defusers'])
+    if numDefuser == 0:
+        random.shuffle(roles['defusers'])
+    defuser = roles['defusers'][numDefuser]
+
+    # Get the round spies
+    spies = []
+    if defuser in roles['spies']:
+        roles['spies'].remove(defuser)
+        spies.extend(roles['spies'])
+        roles['spies'].append(defuser)
+    else:
+        spies.extend(roles['spies'])
+
+    # Get the informants and misinformed
+    informants = []
+    misinformed = []
+    for player in roles['informants']:
+        if player == defuser:
+            continue
+        if random.random() > misRate:
+            informants.append(player)
+        else:
+            misinformed.append(player)
+
+    # Return the round role map
+    roundRoles = {}
+    roundRoles['defuser'] = defuser
+    roundRoles['spies'] = spies
+    roundRoles['informants'] = informants
+    roundRoles['misinformed'] = misinformed
+    return roundRoles
+
 
 # TODO Each round has:
 #  1 bad wire
 #  1-2 good wires
 #  0-1 dead wires
-#color_ids = [1, 2, 4, 5, 7]
-#wire_colors = {1:'red', 2:'green', 4:'blue', 5:'purple', 7:'white'}
-#wire_states = ['good', 'dead', 'bad']
-def doRound(defuser, blockIndex):
+def doRound(roundRoles, blockIndex):
     log.info('Block {}:'.format(blockIndex+1))
-    log.info('  {} is the defuser'.format(defuser))
+    log.info('  {} is the defuser'.format(roundRoles['defuser']))
+    log.debug('  spies = {}, informants = {}, misinformed = {}'.format(
+        roundRoles['spies'], roundRoles['informants'], roundRoles['misinformed']))
+
+    # Choose the wires - 3 per round for now
+    colorIds = [1, 2, 4, 5, 7]
+    wireColors = {1:'red', 2:'green', 4:'blue', 5:'purple', 7:'white'}
+    wireStates = ['good', 'dead', 'bad']
+    random.shuffle(colorIds)
+    colors = colorIds[0:3]
+    log.info('  Wires: \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m \033[1;3{}m|\033[0m'.format(colors[0], colors[1], colors[2]))
+    random.shuffle(wireStates)
+    log.debug('         {} {} {}'.format(wireStates[0][0], wireStates[1][0], wireStates[2][0]))
+
     raw_input('  <enter> to continue\n')
     return 'pass'
 
@@ -70,17 +122,18 @@ def main():
     log.info('   BOMB DEFUSE   ')
     log.info('-----------------')
 
-    (players, roles, missRate) = getPlayersAndRoles(args.numPlayers)
+    (roles, misRate) = getPlayersAndRoles(args.numPlayers)
+    log.debug('spies = {}, informants = {}'.format(roles['spies'], roles['informants']))
+    log.debug('misinformed rate = {}%'.format(misRate))
 
     # Rotate who's being the defuser
     round = 0
     numDefused = 0
     numBlown = 0
     while True:
-        if round % len(players) == 0:
-            random.shuffle(players)
-        defuser = players[round % len(players)]
-        result = doRound(defuser, round % 5)
+        # Get round roles and do round
+        roundRoles = getRoundRoles(round, roles, misRate)
+        result = doRound(roundRoles, round % 5)
         if result == 'defused':
             numDefused += 1
         elif result == 'blown':
